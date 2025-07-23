d2b2114facf5328e7c55e21993ca6e998adaabfd
Arthur Mikael <arthurmik860@gmail.com>
2025-07-22 21:38:03 -0300

    feat [ SEM project created with ready-made frontend, apps and templates ]

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/Slide.java
@@ -0,0 +1,17 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+public interface Slide {
+    
+    String getAlignment();
+
+    String getTitle();
+
+    String getDescription();
+
+    String getButtonName();
+
+    String getInternalLink();
+
+    String getExternalLink();
+
+}
---
+++ ui.frontend/src/main/webpack/site/main.ts
@@ -0,0 +1,11 @@
+// Stylesheets
+import "./main.scss";
+
+// Javascript or Typescript
+import '../components/**/*.js';
+import "./**/*.js";
+import "./**/*.ts";
+
+//Bootstrap
+
+import 'bootstrap';
